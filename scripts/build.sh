03b79ac11448d71c6ec94f3bb568f8025aaeafa7
Philippe Mulet <philippe_mulet@fr.ibm.com>
2017-10-13 18:32:35 +0200

    mint new toolchain token&cluster secret

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #set -x
 
-echo -e "Build environment variables:"
+# env
+echo "Build environment variables:"
 echo "REGISTRY_URL=${REGISTRY_URL}"
 echo "REGISTRY_NAMESPACE=${REGISTRY_NAMESPACE}"
 echo "IMAGE_NAME=${IMAGE_NAME}"
@@ -51,6 +52,21 @@ set +x
 bx cr image-inspect ${IMAGE_LOCATION}:${BUILD_NUMBER}
 
 echo "=========================================================="
+TOKEN_DESCR="bluemix-toolchain-${PIPELINE_TOOLCHAIN_ID}"
+echo "Checking registry token for toolchain: ${TOKEN_DESCR}"
+EXISTING_TOKEN=$(bx cr tokens | grep ${TOKEN_DESCR} ||: )
+if [ -z ${EXISTING_TOKEN} ]; then
+    echo -e "Creating new registry token: ${TOKEN_DESCR}"
+    bx cr token-add --non-expiring --description ${TOKEN_DESCR}
+    REGISTRY_TOKEN_ID=$(bx cr tokens | grep ${TOKEN_DESCR} | awk '{ print $1 }')
+else    
+    echo -e "Reusing existing registry token: ${TOKEN_DESCR}"
+    REGISTRY_TOKEN_ID=$(echo $EXISTING_TOKEN | awk '{ print $1 }')
+fi
+REGISTRY_TOKEN=$(bx cr token-get ${REGISTRY_TOKEN_ID} --quiet)
+echo -e "REGISTRY_TOKEN=${REGISTRY_TOKEN}"
+
+echo "=========================================================="
 echo "Copying artifacts needed for deployment and testing"
 
 echo -e "Checking archive dir presence"
@@ -61,6 +77,10 @@ echo "IMAGE_NAME=${IMAGE_LOCATION}:${BUILD_NUMBER}" >> $ARCHIVE_DIR/build.proper
 
 # RELEASE_NAME from build.properties is used in Helm Chart deployment to set the release name
 echo "RELEASE_NAME=${IMAGE_NAME}" >> $ARCHIVE_DIR/build.properties
+
+# REGISTRY information from build.properties is used in Helm Chart deployment to generate cluster secret
+echo "REGISTRY_URL=${REGISTRY_URL}" >> $ARCHIVE_DIR/build.properties
+echo "REGISTRY_TOKEN=${REGISTRY_TOKEN}" >> $ARCHIVE_DIR/build.properties
 
 # Copy scripts (incl. deploy scripts)
 if [ ! -d $ARCHIVE_DIR/scripts/ ]; then # no need to copy if working in ./ already
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ cat build.properties
 echo "=========================================================="
 echo "Checking cluster"
 IP_ADDR=$(bx cs workers ${PIPELINE_KUBERNETES_CLUSTER_NAME} | grep normal | awk '{ print $2 }')
-if [ -z $IP_ADDR ]; then
+if [ -z ${IP_ADDR} ]; then
   echo -e "${PIPELINE_KUBERNETES_CLUSTER_NAME} not created or workers not ready"
   exit 1
 fi
@@ -21,29 +21,23 @@ else
   echo -e "Namespace ${CLUSTER_NAMESPACE} created."
 fi
 
-echo "=========================================================="
+# Grant access to private image registry from namespace $CLUSTER_NAMESPACE
+# reference https://console.bluemix.net/docs/containers/cs_cluster.html#bx_registry_other
 echo -e "Checking access to private image registry from namespace ${CLUSTER_NAMESPACE}"
-if ! kubectl get secret bluemix-default-secret --namespace ${CLUSTER_NAMESPACE}; then
-  # copy the existing default secret into the new namespace
-  kubectl get secret bluemix-default-secret --namespace default -o yaml |  sed "s~^\([[:blank:]]*\)namespace:.*$~\1namespace: ${CLUSTER_NAMESPACE}~" | kubectl -n ${CLUSTER_NAMESPACE} create -f -
-  # enable default serviceaccount to use the pull secret
-  kubectl patch -n ${CLUSTER_NAMESPACE} serviceaccount/default -p '{"imagePullSecrets":[{"name":"'"bluemix-default-secret"'"}]}'
+IMAGE_PULL_SECRET_NAME="bluemix-toolchain-${PIPELINE_TOOLCHAIN_ID}-${REGISTRY_URL}"
+echo -e "create ${IMAGE_PULL_SECRET_NAME} imagePullSecret if it does not exist"
+if ! kubectl get secret ${IMAGE_PULL_SECRET_NAME} --namespace ${CLUSTER_NAMESPACE}; then
+  echo -e "${IMAGE_PULL_SECRET_NAME} not found in ${CLUSTER_NAMESPACE}, creating it"
+  # for Container Registry, docker username is 'token' and email does not matter
+  kubectl --namespace ${CLUSTER_NAMESPACE} create secret docker-registry ${IMAGE_PULL_SECRET_NAME} --docker-server=${REGISTRY_URL} --docker-password=${REGISTRY_TOKEN} --docker-username=token --docker-email=a@b.com
+  echo "enable default serviceaccount to use the pull secret"
+  kubectl patch -n ${CLUSTER_NAMESPACE} serviceaccount/default -p '{"imagePullSecrets":[{"name":"'"${IMAGE_PULL_SECRET_NAME}"'"}]}'
   echo -e "Namespace ${CLUSTER_NAMESPACE} is now authorized to pull from the private image registry"
 fi
-
-#TODO mint registry secret in host registry, and use it in remove cluster, pull secret should be region specific (ibmcloud-devops-pull-registry-yp:ibm:us-south)
-# Grant access to private image registry from namespace $CLUSTER_NAMESPACE
-#echo "create ${IMAGE_PULL_SECRET_NAME} imagePullSecret if it does not exist"
-#if ! kubectl get secret ${IMAGE_PULL_SECRET_NAME} --namespace ${CLUSTER_NAMESPACE}; then
-#  echo -e "${IMAGE_PULL_SECRET_NAME} not found in ${CLUSTER_NAMESPACE}, creating it"
-#  # for Container Registry, docker username is 'token' and email does not matter
-#  kubectl --namespace ${CLUSTER_NAMESPACE} create secret docker-registry ${IMAGE_PULL_SECRET_NAME} --docker-server=${REGISTRY_HOST} --docker-password=${IMAGE_REGISTRY_TOKEN} --docker-username=token --docker-email=a@b.com
-#  echo "enable default serviceaccount to use the pull secret"
-#  kubectl patch -n ${CLUSTER_NAMESPACE} serviceaccount/default -p '{"imagePullSecrets":[{"name":"'"${IMAGE_PULL_SECRET_NAME}"'"}]}'
-#  echo -e "Namespace ${CLUSTER_NAMESPACE} is now authorized to pull from the private image registry"
-#fi
 echo "default serviceAccount:"
 kubectl get serviceAccount default -o yaml
+
+echo "TODO -- do not patch the account, rather inject secret into the chart"
 
 echo "=========================================================="
 echo "Checking TILLER enabled (Helm's server component)"
