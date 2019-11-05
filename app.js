d1fd21614914173f73fade64673d93ae4542e8c6
Benoit Jaunin <jaunin.b@fr.ibm.com>
2019-11-05 12:08:46 +0100

    Update user, ports to also work in openshift and add reference to openshift guidelines

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /**
-* Copyright 2014 IBM
+* Copyright 2019 IBM
 *
 *   Licensed under the Apache License, Version 2.0 (the "License");
 *   you may not use this file except in compliance with the License.
@@ -11,16 +11,22 @@
 *   distributed under the License is distributed on an "AS IS" BASIS,
 *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 *   See the License for the specific language governing permissions and
+*   limitations under the License.
 **/
 
 var express = require('express');
 
-var PORT = 80;
+var PORT;
+if (process.env.PORT) {
+  PORT = process.env.PORT;
+} else {
+  PORT = 80;
+}
 
 var app = express();
 app.get('/', function (req, res) {
   res.send('Welcome to IBM Cloud DevOps with Docker, Kubernetes and Helm Charts. Lets go use the Continuous Delivery Service');
 });
 
-app.listen(PORT)
+app.listen(PORT);
 console.log(' Application Running on port' + PORT);
