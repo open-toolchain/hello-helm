09c79bdef0d6930e5810289052d18756abaace64
HUA YUEN HUI <HUAYUENH@ie.ibm.com>
2023-10-12 09:50:16 +0100

    chore: update packages (#21)

--- test/fvt-test.js
+++ test/fvt-test.js
@@ -1,11 +1,25 @@
 const expect  = require('chai').expect;
-const request = require('request');
 const message = require('../utils');
+const http = require('http');
 
 
-it('Main page content', function(done) {
-    request('http://localhost:80' , function(error, response, body) {
-        expect(body).to.equal(message.getWelcomeMessage());
-        done();
+it('Main page content', function (done) {
+    const options = {
+        host: 'localhost',
+        path: '/',
+        port: '80'
+    };
+    const request = http.request(options, (response) => {
+        let str = ''
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+
+        response.on('end', function () {
+            console.log(str);
+            expect(str).to.equal(message.getWelcomeMessage());
+            done();
+        });
     });
+    request.end();
 });
