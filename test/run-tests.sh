f5673c9cae3d9d49072ba531f011eeef862791d2
Benoit Jaunin <jaunin.b@fr.ibm.com>
2025-06-23 16:11:25 +0200

    fix: ensure proper exit when failure in tests

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # set -x
+exit_code=0
 npm install
-npm run start-server & npm run test-unit
+npm run start-server & npm run test-unit || exit_code=$?
 mv ./test/jest-junit.xml ./test/unit-test.xml
-npm run test-fvt
+npm run test-fvt || exit_code=$?
 mv ./test/jest-junit.xml ./test/fvt-test.xml
 FILE_LOCATIONS="./test/unit-test.xml;./test/fvt-test.xml"
 TEST_TYPES="unittest;fvt"
+exit $exit_code
